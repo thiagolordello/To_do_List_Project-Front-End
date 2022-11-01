e2c42223df7f17bf0b7f22461c2b4f4a865107dc
Thiago Lordello <thiagolordello@hotmail.com>
2022-10-31 22:00:15 -0300

    first commit to-do-list-front-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import Routs from './router/Routs';
+import Provider from './context/to-do-list-Provider';
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>To Do List</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <Routs />
+    </Provider>
   );
 }
 
